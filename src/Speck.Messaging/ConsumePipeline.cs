1eab82b2f828ac1398f3ac5d778879e5c3d7d02b
Cody Peck <cspeck7@gmail.com>
2025-04-12 18:14:40 -0400

    fix: block on pipeline completion

--- src/Speck.Messaging/ConsumePipeline.cs
+++ src/Speck.Messaging/ConsumePipeline.cs
@@ -4,15 +4,15 @@ using Speck.DataflowExtensions;
 
 namespace Speck.Messaging;
 
-internal class ConsumePipeline<TMessage> : IConsumePipeline
+internal sealed class ConsumePipeline<TMessage> : IConsumePipeline, IAsyncDisposable
 {
     private readonly IServiceScopeFactory _factory;
-    private readonly DataflowPipeline<(TMessage, CancellationToken)> _pipeline;
+    private readonly DataflowPipeline<DataflowItem<(TMessage, CancellationToken)>> _pipeline;
 
     public ConsumePipeline(ConsumerConfiguration consumerConfiguration, IServiceScopeFactory factory)
     {
         _factory = factory;
-        _pipeline = DataflowPipelineBuilder.Create<(TMessage, CancellationToken)>()
+        _pipeline = DataflowPipelineBuilder.Create<DataflowItem<(TMessage, CancellationToken)>>()
             .Build(ConsumeAsync, new ExecutionDataflowBlockOptions
             {
                 BoundedCapacity = consumerConfiguration.BoundedCapacity,
@@ -22,15 +22,31 @@ internal class ConsumePipeline<TMessage> : IConsumePipeline
     
     public async Task SendAsync(object message, CancellationToken cancellationToken)
     {
-        await _pipeline.SendAsync(((TMessage)message, cancellationToken));
+        var dataflowItem = new DataflowItem<(TMessage, CancellationToken)>(((TMessage)message, cancellationToken));
+        await _pipeline.SendAsync(dataflowItem);
+        await dataflowItem.Task;
     }
 
-    private async Task ConsumeAsync((TMessage Message, CancellationToken CancellationToken) item)
+    private async Task ConsumeAsync(DataflowItem<(TMessage Message, CancellationToken CancellationToken)> item)
     {
-        await using var scope = _factory.CreateAsyncScope();
-        
-        var consumer = scope.ServiceProvider.GetRequiredService<IConsumer<TMessage>>();
-        
-        await consumer.ConsumeAsync(item.Message, item.CancellationToken);
+        try
+        {
+            await using var scope = _factory.CreateAsyncScope();
+
+            var consumer = scope.ServiceProvider.GetRequiredService<IConsumer<TMessage>>();
+
+            await consumer.ConsumeAsync(item.Value.Message, item.Value.CancellationToken);
+
+            item.Complete();
+        }
+        catch (Exception exception)
+        {
+            item.Error(exception);
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _pipeline.DisposeAsync();
     }
 }
---
+++ src/Speck.Messaging/DataflowItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Speck.Messaging;
+
+internal class DataflowItem<TValue>(TValue value)
+{
+    private readonly TaskCompletionSource _taskCompletionSource = new();
+    
+    public TValue Value { get; } = value;
+
+    public Task Task => _taskCompletionSource.Task;
+    
+    public void Complete() => _taskCompletionSource.TrySetResult();
+    
+    public void Error(Exception exception) => _taskCompletionSource.TrySetException(exception);
+}
+
